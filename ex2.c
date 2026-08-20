c5562377ea95500914c1938ffb256190e4029c19
Anna Beatriz Fatala Pereira do Carmo <58826708+AnnaFatala@users.noreply.github.com>
2026-08-19 22:01:00 -0300

    Add welcome message program in ex2.c

---
+++ ex2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  printf("Bem-vindos ao Mackenzie\n");  
+  printf("Faculdade Computa e Informática - FCI, desde 1970\n");
+    printf("Algoritmos e\n");
+    printf("Programacao II -\n");
+    printf("Turma 2P11\n");
+
+    return 0;
+}
