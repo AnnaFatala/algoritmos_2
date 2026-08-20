9d4b76cbea4920c4c47e17d51af9481f9df764d1
Anna Beatriz Fatala Pereira do Carmo <58826708+AnnaFatala@users.noreply.github.com>
2026-08-19 22:06:54 -0300

    Initialize ex1_adicional.c with main function

---
+++ ex1_adicional.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+  float salario_min;
+  float salario_bruto;
+  float qtds_sal_min_bruto;
+  printf("Digite o valor do salário minimo:\n");
+  scanf("f", &salario_min);
+  
+}
